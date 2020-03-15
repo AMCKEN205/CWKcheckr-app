8fd409748d7535afb5e9f96c3cb296f0d56fadd1
James Devenport <Jdeven200@caledonian.ac.uk>
2020-03-15 19:43:56 +0000

    Initial UI screens for Login, Register, View Coursework and Homepage screens

--- DB_interaction/models.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const schemas = require("./schemas")
-
-// define the models used to store schema conforming data
-var model_name = "Student"
-var collection_name = "Students"
-var Student = mongoose.model(model_name, schemas.student_schema, collection_name)
-
-module.exports = {Student : Student}
