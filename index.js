0327a04b6892de691c821314a2702d0f785d2666
oheneba <ohenebakob@hotmail.com>
2020-03-15 03:09:41 +0000

    init server and create endpoint controller for /api/users

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const bodyParser = require('body-parser')
+const app = express();
+
+//This is middle ware that logs each request when it is received
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('----------------------')
+
+  next()
+}
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+app.use(requestLogger)//log requests
+app.use(express.json())
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+
+app.get('/', (req, res) => {
+  //we will render the homepage here
+  res.send('<h1>Hello World!</h1>')
+})
+app.get('/api', (req, res) => {
+  res.send('<p>all server calls will come to api /something</p>')
+})
+app.post('/api/login', (req, res) => {
+  res.send('<p>login</p>')
+})
+
+
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, ()=> {
+  console.log(`Server is running on port ${PORT}`)
+})
