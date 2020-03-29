9bd97fc5995f74066ba760227ff2695f17cffe7f
Alex <AMCKEN205@caledonian.ac.uk>
2020-03-29 15:42:07 +0100

    Code formatting

--- DB_interaction/db-accessor-test-script.js
+++ DB_interaction/db-accessor-test-script.js
@@ -14,26 +14,33 @@ dao.get_model_items(db_accessor.models.Coursework).then(courseworks => {
     console.log(courseworks)
 });
 
-// dao.add_student(
-//     1, 
-//     "Bob", 
-//     "Bobby", 
-//     "1234", 
-//     [
-//         {
-//             parent_course_no : 1234, 
-//             coursework_title : "Web dev cwk 1", 
-//             coursework_description : "the first coursework"
-//     }
-//     ],
-//     [
-//         {
-//             course_no : 1234,
-//             course_name : "Web Platform Development 2"
-//         }
-//     ]
+dao.add_student(
+    1, 
+    "Bob", 
+    "Bobby", 
+    "1234", 
+    [
+        {
+            parent_course_no : 1234, 
+            coursework_title : "Web dev cwk 1", 
+            coursework_description : "the first coursework"
+    }
+    ],
+    [
+        {
+            course_no : 1234,
+            course_name : "Web Platform Development 2"
+        }
+    ]
     
-//     );
+    );
+
+dao.add_course(
+    1,
+    "test course",
+    "test teacher",
+    "test description"
+)
 
 //should fail
 dao.add_coursework(
--- DB_interaction/db-accessor.js
+++ DB_interaction/db-accessor.js
@@ -7,6 +7,7 @@ class DAO {
     constructor(){    
         // Mongo server used to host the applications MongoDB.
         this.db_url = process.env.MONGODB_URI
+
         // Indicate the number of running db processes, 
         // used to indicate when to start and stop processes.
         // Means we don't initalise the db connection when it's already running,
@@ -21,10 +22,13 @@ class DAO {
         // Add new students to the database
         
         this._init_db();
-        // Indicate the add_student process is currently running.
+
+        // Indicate we've stopped running the add student process.
         var add_student_run_indicator = "add_student"
         this.process_queue.push(add_student_run_indicator)
-        var student_to_add = new models.Student({
+
+        var student_to_add = new models.Student
+        ({
             studentNo : studentNo,
             name : name,
             username : username,
@@ -34,20 +38,22 @@ class DAO {
         });
 
         student_to_add.save()
-        .then(() => {
+        .then(() => 
+        {
             console.log(`student ${student_to_add.studentNo} saved to students collection.`);
         })
-        .catch(err => {
+        .catch(err => 
+        {
             console.log(err)
             console.log(`student ${student_to_add.studentNo} failed to save to students collection, see error above.`);
         })
-        .then(() => {
+        .then(() => 
+        {
             // Always close the database connection, 
             // regardless as to the success or failure of the operation
             
-            // Indicate the add_student process has finished running.
+            // Indicate we've stopped running the add student process.
             this.process_queue.shift()
-
             this._close_db_connection();
         });
     }
@@ -57,11 +63,13 @@ class DAO {
         
         this._init_db();
 
+        // Indicate we've started running the get model items process. 
         var add_course_run_indicator = "add_course"
 
         this.process_queue.push(add_course_run_indicator)
 
-        var course_to_add = new models.Course({
+        var course_to_add = new models.Course
+        ({
             courseId : courseId,
             courseName : courseName,
             courseTeacher : courseTeacher,
@@ -69,33 +77,38 @@ class DAO {
         });
 
         course_to_add.save()
-        .then(() => {
+        .then(() => 
+        {
             console.log(`course ${course_to_add.courseId} saved to courses collection.`);
         })
-        .catch(err => {
+        .catch(err => 
+        {
             console.log(err)
             console.log(`course ${course_to_add.courseId} failed to save to courses collection, see error above.`);
         })
-        .then(() => {
+        .then(() => 
+        {
             // Always close the database connection, 
             // regardless as to the success or failure of the operation
             
-            // Indicate the add course process has finished running.
+            // Indicate we've stopped running the add course process.
             this.process_queue.shift()
-
             this._close_db_connection();
         });
     }
 
     add_coursework(courseworkId, courseId, courseworkName, courseworkDescription, dueDate) {
         // Add new courseworks to the database
-        
 
+        // Indicate we've started running the add coursework process. 
         var add_coursework_run_indicator = "add_coursework"
 
         this.process_queue.push(add_coursework_run_indicator)
 
-        var coursework_to_add = new models.Coursework({
+        this._init_db()
+
+        var coursework_to_add = new models.Coursework
+        ({
             courseworkId : courseworkId,
             courseId : courseId,
             courseworkName : courseworkName,
@@ -103,41 +116,39 @@ class DAO {
             dueDate : dueDate
         });
         
-        var get_course_ids_find_doc = {"courseId" : {$in : [
-            courseId
-            ]}
+        var get_course_ids_find_doc = 
+        {
+            "courseId" : {$in : [courseId]}
         };
+
         var get_course_ids_projection_doc = {_id : 0, courseId : 1}
 
-        return new Promise((resolve,reject) => {
-            this.get_model_items(models.Course, get_course_ids_find_doc, get_course_ids_projection_doc)
-            .then(courseIds => {
-                if (courseIds.length == 0){
-                    throw "Attempted to add a coursework not linked to an existing course!";
-                }
-                else{
-                    this._init_db()
-                    .then(() => {
-                        coursework_to_add.save();
-                        console.log(`coursework ${coursework_to_add.courseId} saved to courseworks collection.`);
-                        resolve(true)
-                    });
-                }
-            })
-            .catch(err => {
-                console.log(err)
-                console.log(`coursework ${coursework_to_add.courseId} failed to save to courseworks collection, see error above.`);
-                reject(false)
-            })
-            .then(() => {
+        this.get_model_items(models.Course, get_course_ids_find_doc, get_course_ids_projection_doc)
+        .then(courseIds => {
+            if (courseIds.length == 0)
+            {
+                throw "Attempted to add a coursework not linked to an existing course!";
+            }
+            else
+            {
+                coursework_to_add.save();
+                console.log(`coursework ${coursework_to_add.courseId} saved to courseworks collection.`);
+            }
+        })
+        .catch(err => 
+        {
+            console.log(err)
+            console.log(`coursework ${coursework_to_add.courseId} failed to save to courseworks collection, see error above.`);
+        })
+        .then(() => 
+        {
             // Always close the database connection, 
             // regardless as to the success or failure of the operation
             
+            // Indicate we've stopped running the add coursework process. 
             this.process_queue.shift()
             this._close_db_connection();
-            });
-        })
-         
+        });
     }
 
     get_model_items(model, query_doc={}, projection_doc=null) {
@@ -150,29 +161,33 @@ class DAO {
         // projection_doc == mongo query used to find specific fields of model
         // db entries found. Null == find every item with every field.
         
-
+        // Indicate we've started running the get model items process. 
         var get_model_items_run_indicator = "get_model_items"
 
         this.process_queue.push(get_model_items_run_indicator)
+        this._init_db()
 
-        return new Promise((resolve, reject) => {
-            this._init_db()
-            .then(() => {
-                model.find(query_doc, projection_doc)
-                .then(function(entries) {
-                    resolve(entries);
-                })
-                .catch(function(err) {
-                    console.log(err);
-                    console.log(`Failed to list model entries, see error above.`);
-                    reject(null);
-                })
-                .then(() => {
-                    // Always close the database connection, 
-                    // regardless as to the success or failure of the operation
-                    this.process_queue.shift()
-                    this._close_db_connection()
-                });
+        return new Promise((resolve, reject) => 
+        {
+            model.find(query_doc, projection_doc)
+            .then(function(entries) 
+            {
+                resolve(entries);
+            })
+            .catch(function(err) 
+            {
+                console.log(err);
+                console.log(`Failed to list model entries, see error above.`);
+                reject(null);
+            })
+            .then(() => 
+            {
+                // Always close the database connection, 
+                // regardless as to the success or failure of the operation
+                
+                // Indicate we've stopped running the get model items process. 
+                this.process_queue.shift()
+                this._close_db_connection()
             });
         });
     }
@@ -181,39 +196,42 @@ class DAO {
 
     _init_db(){
         // Initalise the connection to the application mongoDB database
-        
-        return new Promise((resolve,reject) => {
-            // 
-            if (this.process_queue.length > 1){
-                resolve(true)
-                return
-            }
-            mongoose.connect(this.db_url, {useNewUrlParser: true, useUnifiedTopology: true})
-            .then(() => {
-                // Database connection success
-                console.log("Application database connection success!");
-                resolve(true)
-            })
-            .catch((err) => {
-                // Database connection failure
-                console.log(console.log(err));
-                console.log("Application database connection failure! See error details above.");
-                reject(false)
-            });
+        // Check we're on the first init db connection call, otherwise don't open a new connection.
+        if (this.process_queue.length > 1)
+        {
+            return
+        }
+        mongoose.connect(this.db_url, {useNewUrlParser: true, useUnifiedTopology: true})
+        .then(() => 
+        {
+            // Database connection success
+            console.log("Application database connection success!");
+        })
+        .catch((err) => 
+        {
+            // Database connection failure
+            console.log(console.log(err));
+            console.log("Application database connection failure! See error details above.");
         });
     }
 
     _close_db_connection() {
-        if (this.process_queue.length > 0){
+        // Close the connection to the applications MongoDB database.
+
+        // Check we're on the last close db connection call, otherwise don't close the connection.
+        if (this.process_queue.length > 0)
+        {
             return
         }
         // Close the mongo db connection
         mongoose.connection.close()
-        .then(() => {
+        .then(() => 
+        {
             // Disconnect success
             console.log("Application database disconnect success!");
         })
-        .catch((err) => {
+        .catch((err) => 
+        {
             // Disconnect failure
             console.log(console.log(err));
             console.log("Application database disconnect failure! See error details above.");
